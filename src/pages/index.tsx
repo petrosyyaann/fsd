cd68e0102836c600e2381ae6831e181a761e8cac
Ningaro <23503775+Ningaro@users.noreply.github.com>
2023-04-03 00:33:57 +0300

    ✨ Add pretty spinner for loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
+import LoadingPage from './loading'
+
 const ReactError = lazy(() => import('./react-error'))
 const TestPage = lazy(() => import('./test'))
 const TestErrorPage = lazy(() => import('./test-error'))
@@ -10,6 +12,7 @@ export default function Routing() {
   return (
     <Routes>
       <Route path="/" element={<TestPage />} />
+      <Route path="/1" element={<TestPage />} />
       <Route path="/error" element={<TestErrorPage />} />
       <Route path="/*" element={<Error404Page />} />
     </Routes>
---
+++ src/pages/loading/index.tsx
@@ -0,0 +1,11 @@
+import { Center } from '@mantine/core'
+
+import { Loader } from '../../shared'
+
+const LoadingPage = () => (
+  <Center h="100vh">
+    <Loader size="lg" />
+  </Center>
+)
+
+export default LoadingPage
